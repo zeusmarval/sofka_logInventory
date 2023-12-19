ddb1c7c331b27d9a128e4b743eabd669cdfdd47a
zeusmarval <zeusmarval@gmail.com>
2023-12-19 01:11:11 -0400

    Initial Commit

---
+++ src/main/java/com/zama/logsInventori/LogsInventoriApplication.java
@@ -0,0 +1,13 @@
+package com.zama.logsInventori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsInventoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogsInventoriApplication.class, args);
+	}
+
+}
